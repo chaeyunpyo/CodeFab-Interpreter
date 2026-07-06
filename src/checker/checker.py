2dedd9bb019d934e08774d8718f3c0fdd754ef95
yunpyo.chae <yunpyo.chae@samsung.com>
2026-07-06 16:41:02 +0900

    [Refactor] checker 코드 리펙터링_1

--- src/checker/checker.py
+++ src/checker/checker.py
@@ -11,14 +11,9 @@ Stmt 목록을 받아서 하나씩 순서대로 검사한다. 블록(BlockStmt)�
 라고 부른다.
 """
 
-from nodes.expr import (
-    AssignExpr,
-    BinaryExpr,
-    GroupingExpr,
-    LogicalExpr,
-    UnaryExpr,
-    VariableExpr,
-)
+import dataclasses
+
+from nodes.expr import Expr, VariableExpr
 from nodes.stmt import BlockStmt, ForStmt, IfStmt, VarDeclStmt
 
 
@@ -44,18 +39,11 @@ class CheckerUnit:
         return errors
 
     def check_statements(self, statements, errors):
-        """statement 리스트 하나를 '블록(스코프)' 하나로 보고 순서대로 검사한다.
-
-        declared_names는 지금까지 이 블록에서 선언된 변수 이름들을 담아두는
-        리스트다. 새 블록에 들어올 때마다 새로 만들어지므로, 다른 블록의
-        변수 이름과는 서로 겹쳐도 문제가 되지 않는다.
-        """
         declared_names = []
         for statement in statements:
             self.check_statement(statement, declared_names, errors)
 
     def check_statement(self, statement, declared_names, errors):
-        """statement 하나를 검사한다."""
 
         if isinstance(statement, VarDeclStmt):
             self.check_var_decl(statement, declared_names, errors)
@@ -92,32 +80,12 @@ class CheckerUnit:
             declared_names.append(name)
 
     def expr_uses_name(self, expr, name):
-        """expr 안 어딘가에서 name이라는 변수를 사용하는지 검사한다.
-
-        Expr도 트리 구조이기 때문에 하위 Expr을 하나씩 재귀적으로 확인한다.
-        """
-
         if isinstance(expr, VariableExpr):
             return expr.name.lexeme == name
 
-        if isinstance(expr, AssignExpr):
-            return self.expr_uses_name(expr.value, name)
+        for field in dataclasses.fields(expr):
+            value = getattr(expr, field.name)
+            if isinstance(value, Expr) and self.expr_uses_name(value, name):
+                return True
 
-        if isinstance(expr, UnaryExpr):
-            return self.expr_uses_name(expr.right, name)
-
-        if isinstance(expr, BinaryExpr):
-            left_uses_name = self.expr_uses_name(expr.left, name)
-            right_uses_name = self.expr_uses_name(expr.right, name)
-            return left_uses_name or right_uses_name
-
-        if isinstance(expr, LogicalExpr):
-            left_uses_name = self.expr_uses_name(expr.left, name)
-            right_uses_name = self.expr_uses_name(expr.right, name)
-            return left_uses_name or right_uses_name
-
-        if isinstance(expr, GroupingExpr):
-            return self.expr_uses_name(expr.expression, name)
-
-        # LiteralExpr처럼 더 이상 하위 Expr이 없는 경우 name을 쓸 수 없다.
         return False
